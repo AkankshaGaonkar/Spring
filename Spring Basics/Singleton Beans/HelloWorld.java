144b0c517e06b801ee13ecc5341179e62161002d
Akanksha <ashugaonkar2000@gmail.com>
2022-06-05 21:56:33 +0530

    Create HelloWorld.java

---
+++ Spring Basics/Singleton Beans/HelloWorld.java
@@ -0,0 +1,17 @@
+package com.spring;
+
+public class HelloWorld
+{
+		   public String s;
+		   public void getMessage()
+		   {
+		      System.out.println("Your Name is : " + s);
+		   }
+		   public void setMessage(String message)
+		   {
+		      this.s  = message;
+		   }
+		   
+}
+
+

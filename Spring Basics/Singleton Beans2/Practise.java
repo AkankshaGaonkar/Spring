deeabe733ce131d719538e50b93a188092057b02
Akanksha <ashugaonkar2000@gmail.com>
2022-06-06 06:38:59 +0530

    Update Practise.java

--- Spring Basics/Singleton Beans2/Practise.java
+++ Spring Basics/Singleton Beans2/Practise.java
@@ -1,16 +1,16 @@
 package com.example;
 
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.support.ClassPathXmlApplicationContext;
-
-public class Main 
+public class Practice
 {
-	  public static void main(String[] args) 
-	  {
-	      ApplicationContext context = new ClassPathXmlApplicationContext("app.xml"); 
-	      Practice obj = (Practice) context.getBean("beanOne"); 
-	      Practice object = (Practice) context.getBean("beanTwo");
-	      obj.getName();
-	      object.getName();
-	  }
+   public String n;
+   public void getName()
+   {
+      System.out.println("Your Name is : " + n);
+
+   }
+
+   public void setName(String name)
+   {
+      this.n  = name;
+   }
 }
